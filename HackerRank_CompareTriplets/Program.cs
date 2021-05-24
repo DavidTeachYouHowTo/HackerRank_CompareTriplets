6044e51630219de48a9ca1c82e0ab48a02d6e21f
profesordavidvaldez <42122724+profesordavidvaldez@users.noreply.github.com>
2021-05-24 14:34:20 -0400

    Add project files.

---
+++ HackerRank_CompareTriplets/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank_CompareTriplets
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            List<int> a = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(aTemp => Convert.ToInt32(aTemp)).ToList();
+
+            List<int> b = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(bTemp => Convert.ToInt32(bTemp)).ToList();
+
+            List<int> result = Result.compareTriplets(a, b);
+
+            Console.WriteLine(String.Join(" ", result));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ HackerRank_CompareTriplets/Result.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+
+namespace HackerRank_CompareTriplets
+{
+    public class Result
+    {
+
+        /*
+         * Complete the 'compareTriplets' function below.
+         *
+         * The function is expected to return an INTEGER_ARRAY.
+         * The function accepts following parameters:
+         *  1. INTEGER_ARRAY a
+         *  2. INTEGER_ARRAY b
+         */
+
+        public static List<int> compareTriplets(List<int> a, List<int> b)
+        {
+            int alicePoints = 0;
+            int bobPoints = 0;
+
+            List<int> result = new List<int>();
+
+            for (int i = 0; i < a.Count; i++)
+            {
+                int firstValue = a[i];
+                int secondValue = b[i];
+
+                if (firstValue > secondValue)
+                {
+                    alicePoints++;
+                }
+                else if(firstValue < secondValue)
+                {
+                    bobPoints++;
+                }
+
+            }
+
+            result.Add(alicePoints);
+            result.Add(bobPoints);
+
+            return result;
+        }
+
+    }
+}
